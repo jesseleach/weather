6d13b28164bf5e18920867a90820cd6f3a341680
jesseleach <jesserobertleach@gmail.com>
2023-02-12 10:33:12 -0500

    before grid

--- src/App.js
+++ src/App.js
@@ -8,14 +8,45 @@ function App() {
         <h1>{title}</h1>
         <p>{subtitle}</p>
         <div className="Pictures" id="flex">
-          <img className="box" src="https://blog-www.pods.com/wp-content/uploads/2019/04/MG_1_1_New_York_City-1.jpg"/>
-          <img className="box" src="https://media.istockphoto.com/id/1223692341/photo/top-view-of-the-embankment-of-the-neckar-river-bridges-tv-tower-green-grass-and-trees.jpg?s=612x612&w=0&k=20&c=IxWnbsuu2tDLt07fTVWV7pGis9bdpFzfZmERdarz6lw="/>
-          <img className="box" src="https://media.istockphoto.com/id/635980782/photo/jacksonville-florida-skyline.jpg?s=612x612&w=0&k=20&c=RxzHa9J6kFYF4E3J-HUVGMnb-fWEbsRZ7-QI0oRJen0="/>
-        </div>
+          <div className="image-container">
+          <div className="center">New York City</div>
+            <img
+              className="box"
+              src="https://blog-www.pods.com/wp-content/uploads/2019/04/MG_1_1_New_York_City-1.jpg"
+            ></img>
+          </div>
+
+          <div className="image-container">
+            <div className="center">Mannheim</div>
+          <img
+            className="box"
+            src="https://media.istockphoto.com/id/1223692341/photo/top-view-of-the-embankment-of-the-neckar-river-bridges-tv-tower-green-grass-and-trees.jpg?s=612x612&w=0&k=20&c=IxWnbsuu2tDLt07fTVWV7pGis9bdpFzfZmERdarz6lw="
+          />
+</div>
+
+<div className="image-container">
+            <div className="center">Jacksonville</div>
+          <img
+            className="box"
+            src="https://www.hellolanding.com/blog/wp-content/uploads/2021/09/image2-12-1200x800.jpg"
+          />
+</div>
+
+
+</div>     
+        
         <div className="display">
-          <h1>Display</h1>
-        </div>
+        <img
+        className="weather"
+        src="https://cloudfront-us-east-1.images.arcpublishing.com/gray/6DQSYHUBHNHVBKLHCRKC6WYDVI.jpg"
+        />
+     <h2>weather</h2>
       </div>
+      </div>
+      <footer>
+      <p>words</p>
+      </footer>
+     
     </div>
   );
 }
