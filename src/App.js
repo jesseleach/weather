d5421d8e228bc20a8e8523acfcb7098f8599cc98
jesseleach <jesserobertleach@gmail.com>
2023-04-01 15:48:33 -0400

    api

--- src/App.js
+++ src/App.js
@@ -1,52 +1,73 @@
+import { useEffect } from "react";
+import {useState} from "react";
+import axios from "axios";
+
 function App() {
   const title = "Weather App";
   const subtitle = "Weather Report from Some of My Favorite Cities";
 
+  const [data, setData,] = useState ({})
+  const [location, setLocation] = useState ('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=2c70d56cc162c3f7b55f03952b1432aa`; 
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+  }
+
   return (
     <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <p>{subtitle}</p>
-        <div className="Pictures" id="flex">
-          <div className="image-container">
-          <div className="center">New York City</div>
-            <img
-              className="box"
-              src="https://blog-www.pods.com/wp-content/uploads/2019/04/MG_1_1_New_York_City-1.jpg"
-            ></img>
-          </div>
-
-          <div className="image-container">
-            <div className="center">Mannheim</div>
-          <img
-            className="box"
-            src="https://media.istockphoto.com/id/1223692341/photo/top-view-of-the-embankment-of-the-neckar-river-bridges-tv-tower-green-grass-and-trees.jpg?s=612x612&w=0&k=20&c=IxWnbsuu2tDLt07fTVWV7pGis9bdpFzfZmERdarz6lw="
-          />
-</div>
-
-<div className="image-container">
-            <div className="center">Jacksonville</div>
-          <img
-            className="box"
-            src="https://www.hellolanding.com/blog/wp-content/uploads/2021/09/image2-12-1200x800.jpg"
-          />
-</div>
-
-
-</div>     
-        
-        <div className="display">
-        <img
-        className="weather"
-        src="https://cloudfront-us-east-1.images.arcpublishing.com/gray/6DQSYHUBHNHVBKLHCRKC6WYDVI.jpg"
-        />
-     <h2>weather</h2>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+      <div className="search">
+        <input 
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type="text"/>
       </div>
+      <div className="grid stretch">
+        <div className="blur pic">
+          <img src="https://media.istockphoto.com/id/599766748/photo/the-city-of-dreams-new-york-citys-skyline-at-twilight.jpg?s=612x612&w=0&k=20&c=Xl4svBmlDj67f78XK649lRiioherRzPG3KQ3qsUi0bo=" />
+          <div className="words">
+            <span>New York City</span>
+          </div>
+        </div>
+
+        <div className="blur pic">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/Der_Friedrichsplatz_und_der_Wasserturm.jpg" />
+          <div className="words">
+            <span>Mannheim</span>
+          </div>
+        </div>
+
+        <div className="blur pic">
+          <img src="https://www.hellolanding.com/blog/wp-content/uploads/2021/09/image2-12-1200x800.jpg" />
+          <div className="words">
+            <span>Jacksonville</span>
+          </div>
+        </div>
+
+        <div className="display">
+          <div className="weather">
+          <p>{data.name}</p>
+          <p>{data.main.temp}°F</p>
+          <p>{data.main.humidity}</p>
+          <p>{data.wind.speed}</p>
+          </div>
+        </div>
+       
       </div>
       <footer>
-      <p>words</p>
+        <p>Jesse Leach 2023</p>
       </footer>
-     
     </div>
   );
 }
